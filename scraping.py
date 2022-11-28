76d21079ca174f80637d92d0afd70b9e4c49ecc1
T$uru$hima <67181317+SotaTsurushima@users.noreply.github.com>
2022-11-28 19:33:20 +0900

    全てのaタグを抽出するよう修正

--- scraping.py
+++ scraping.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 vgm_url = 'https://www.vgmusic.com/music/console/nintendo/nes/'
 html_text = requests.get(vgm_url).text
 soup = BeautifulSoup(html_text, 'html.parser')
-print(soup)
+# 全てのaタグを抽出
+elems = soup.find_all(href=re.compile("http://www.vgmusic.com/"))
+print()
