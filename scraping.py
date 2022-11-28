be76d182941fc1900cca1070a8807e9e44373900
T$uru$hima <67181317+SotaTsurushima@users.noreply.github.com>
2022-11-28 19:35:45 +0900

    タイトルとURLが出力されるよう修正

--- scraping.py
+++ scraping.py
@@ -7,4 +7,7 @@ html_text = requests.get(vgm_url).text
 soup = BeautifulSoup(html_text, 'html.parser')
 # 全てのaタグを抽出
 elems = soup.find_all(href=re.compile("http://www.vgmusic.com/"))
-print()
+
+for elem in elems:
+  print(elem.contents[0])
+  print(elem.attrs['href'])
